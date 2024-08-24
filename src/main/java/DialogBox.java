b73af466e5cc113b329c1aa76732518bd3bc51ad
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-08-24 23:05:46 +0800

    Completed Tutorial 3

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,8 +1,13 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
 
 public class DialogBox extends HBox {
 
@@ -17,7 +22,28 @@ public class DialogBox extends HBox {
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
         this.setAlignment(Pos.TOP_RIGHT);
+        Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
 
         this.getChildren().addAll(text, displayPicture);
+        this.setPadding(new Insets(10));
+        this.setStyle("-fx-background-color: #f0f8ff; -fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #0000ff; -fx-border-width: 2;");
+    }
+
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getDukeDialog(String s, Image i) {
+        var db = new DialogBox(s, i);
+        db.flip();
+        return db;
     }
 }
